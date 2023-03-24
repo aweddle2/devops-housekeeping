dd096044287bada62991634ab697cbae478986cf
Andrew Weddle <aweddle@gmail.com>
2023-03-24 20:56:42 +1100

    boilerplate angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SettingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
